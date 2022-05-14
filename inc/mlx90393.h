f9b1b7ef74b9e2a3271504cb8d88340842e8dc1b
lgnq <dzzxzz@gmail.com>
2022-05-14 13:50:27 +0800

    Update mlx90393.h

--- inc/mlx90393.h
+++ inc/mlx90393.h
@@ -12,7 +12,9 @@
 #define __MLX90393_H__
 
 #include <rtthread.h>
-#include <stdint.h>
+
+#define MLX90393_I2C_ADDRESS                    0x0C        // address pin A0,A1 low (GND), default for MLX90393
+// #define MLX90393_I2C_ADDRESS                    (0x18 >> 1) // 7-bit address pin A0,A1 low (GND), default for MLX90393
 
 enum cmd
 {
@@ -63,6 +65,123 @@ enum mlx90393_cmd
                             Sample Rate = [1000 -  4]HZ when dlpf is enable
                             Sample Rate = [8000 - 32]HZ when dlpf is disable */
     MPU6XXX_SLEEP        /* Sleep mode */
+};
+
+/** HALLCONF settings for CONF1 register. */
+typedef enum mlx90393_hallconf 
+{
+    MLX90393_HALLCONF_0 = (0x0),
+    MLX90393_HALLCONF_C = (0xC),
+} mlx90393_hallconf_t;
+
+/** Gain settings for CONF1 register. */
+typedef enum mlx90393_gain 
+{
+    MLX90393_GAIN_5X = (0x00),
+    MLX90393_GAIN_4X,
+    MLX90393_GAIN_3X,
+    MLX90393_GAIN_2_5X,
+    MLX90393_GAIN_2X,
+    MLX90393_GAIN_1_67X,
+    MLX90393_GAIN_1_33X,
+    MLX90393_GAIN_1X
+} mlx90393_gain_t;
+
+/** Resolution settings for CONF3 register. */
+typedef enum mlx90393_resolution 
+{
+    MLX90393_RES_16,
+    MLX90393_RES_17,
+    MLX90393_RES_18,
+    MLX90393_RES_19,
+} mlx90393_resolution_t;
+
+/** Digital filter settings for CONF3 register. */
+typedef enum mlx90393_filter 
+{
+    MLX90393_FILTER_0,
+    MLX90393_FILTER_1,
+    MLX90393_FILTER_2,
+    MLX90393_FILTER_3,
+    MLX90393_FILTER_4,
+    MLX90393_FILTER_5,
+    MLX90393_FILTER_6,
+    MLX90393_FILTER_7,
+} mlx90393_filter_t;
+
+/** Oversampling settings for CONF3 register. */
+typedef enum mlx90393_oversampling 
+{
+    MLX90393_OSR_0,
+    MLX90393_OSR_1,
+    MLX90393_OSR_2,
+    MLX90393_OSR_3,
+} mlx90393_oversampling_t;
+
+/** Lookup table to convert raw values to uT based on [HALLCONF][GAIN_SEL][RES].
+ */
+const float mlx90393_lsb_lookup[2][8][4][2] = 
+{
+    /* HALLCONF = 0xC (default) */
+    {
+        /* GAIN_SEL = 0, 5x gain */
+        {{0.751, 1.210}, {1.502, 2.420}, {3.004, 4.840}, {6.009, 9.680}},
+        /* GAIN_SEL = 1, 4x gain */
+        {{0.601, 0.968}, {1.202, 1.936}, {2.403, 3.872}, {4.840, 7.744}},
+        /* GAIN_SEL = 2, 3x gain */
+        {{0.451, 0.726}, {0.901, 1.452}, {1.803, 2.904}, {3.605, 5.808}},
+        /* GAIN_SEL = 3, 2.5x gain */
+        {{0.376, 0.605}, {0.751, 1.210}, {1.502, 2.420}, {3.004, 4.840}},
+        /* GAIN_SEL = 4, 2x gain */
+        {{0.300, 0.484}, {0.601, 0.968}, {1.202, 1.936}, {2.403, 3.872}},
+        /* GAIN_SEL = 5, 1.667x gain */
+        {{0.250, 0.403}, {0.501, 0.807}, {1.001, 1.613}, {2.003, 3.227}},
+        /* GAIN_SEL = 6, 1.333x gain */
+        {{0.200, 0.323}, {0.401, 0.645}, {0.801, 1.291}, {1.602, 2.581}},
+        /* GAIN_SEL = 7, 1x gain */
+        {{0.150, 0.242}, {0.300, 0.484}, {0.601, 0.968}, {1.202, 1.936}},
+    },
+
+    /* HALLCONF = 0x0 */
+    {
+        /* GAIN_SEL = 0, 5x gain */
+        {{0.787, 1.267}, {1.573, 2.534}, {3.146, 5.068}, {6.292, 10.137}},
+        /* GAIN_SEL = 1, 4x gain */
+        {{0.629, 1.014}, {1.258, 2.027}, {2.517, 4.055}, {5.034, 8.109}},
+        /* GAIN_SEL = 2, 3x gain */
+        {{0.472, 0.760}, {0.944, 1.521}, {1.888, 3.041}, {3.775, 6.082}},
+        /* GAIN_SEL = 3, 2.5x gain */
+        {{0.393, 0.634}, {0.787, 1.267}, {1.573, 2.534}, {3.146, 5.068}},
+        /* GAIN_SEL = 4, 2x gain */
+        {{0.315, 0.507}, {0.629, 1.014}, {1.258, 2.027}, {2.517, 4.055}},
+        /* GAIN_SEL = 5, 1.667x gain */
+        {{0.262, 0.422}, {0.524, 0.845}, {1.049, 1.689}, {2.097, 3.379}},
+        /* GAIN_SEL = 6, 1.333x gain */
+        {{0.210, 0.338}, {0.419, 0.676}, {0.839, 1.352}, {1.678, 2.703}},
+        /* GAIN_SEL = 7, 1x gain */
+        {{0.157, 0.253}, {0.315, 0.507}, {0.629, 1.014}, {1.258, 2.027}},
+    }};
+
+/** Lookup table for conversion time based on [DIF_FILT][OSR].
+ */
+const float mlx90393_tconv[8][4] = 
+{
+    /* DIG_FILT = 0 */
+    {1.27, 1.84, 3.00, 5.30},
+    /* DIG_FILT = 1 */
+    {1.46, 2.23, 3.76, 6.84},
+    /* DIG_FILT = 2 */
+    {1.84, 3.00, 5.30, 9.91},
+    /* DIG_FILT = 3 */
+    {2.61, 4.53, 8.37, 16.05},
+    /* DIG_FILT = 4 */
+    {4.15, 7.60, 14.52, 28.34},
+    /* DIG_FILT = 5 */
+    {7.22, 13.75, 26.80, 52.92},
+    /* DIG_FILT = 6 */
+    {13.36, 26.04, 51.38, 102.07},
+    /* DIF_FILT = 7 */
+    {25.65, 50.61, 100.53, 200.37},
 };
 
 union mlx90393_status
@@ -169,7 +288,7 @@ union mlx90393_register3
 /* 3-axis data structure */
 struct mlx90393_txyz
 {
-    rt_int16_t t;
+    rt_uint16_t t;
     rt_int16_t x;
     rt_int16_t y;
     rt_int16_t z;
@@ -292,5 +411,8 @@ rt_err_t mlx90393_get_gyro_offset(struct mlx90393_device *dev, struct mlx90393_3
 rt_err_t mlx90393_nop(struct mlx90393_device *dev);
 rt_err_t mlx90393_exit(struct mlx90393_device *dev);
 rt_err_t mlx90393_reset(struct mlx90393_device *dev);
+
+rt_err_t mlx90393_get_gain_sel(struct mlx90393_device *dev, mlx90393_gain_t *gain);
+rt_err_t mlx90393_get_resolution(struct mlx90393_device *dev, mlx90393_resolution_t *res_x, mlx90393_resolution_t *res_y, mlx90393_resolution_t *res_z);
 
 #endif
