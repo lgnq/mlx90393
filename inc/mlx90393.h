9e1fb79f94cc78be04a3ff6c7e2a5eec62d86d15
lgnq <dzzxzz@gmail.com>
2022-05-16 23:04:02 +0800

    Update mlx90393.h

--- inc/mlx90393.h
+++ inc/mlx90393.h
@@ -118,72 +118,6 @@ typedef enum mlx90393_oversampling
     MLX90393_OSR_3,
 } mlx90393_oversampling_t;
 
-/** Lookup table to convert raw values to uT based on [HALLCONF][GAIN_SEL][RES].
- */
-const float mlx90393_lsb_lookup[2][8][4][2] = 
-{
-    /* HALLCONF = 0xC (default) */
-    {
-        /* GAIN_SEL = 0, 5x gain */
-        {{0.751, 1.210}, {1.502, 2.420}, {3.004, 4.840}, {6.009, 9.680}},
-        /* GAIN_SEL = 1, 4x gain */
-        {{0.601, 0.968}, {1.202, 1.936}, {2.403, 3.872}, {4.840, 7.744}},
-        /* GAIN_SEL = 2, 3x gain */
-        {{0.451, 0.726}, {0.901, 1.452}, {1.803, 2.904}, {3.605, 5.808}},
-        /* GAIN_SEL = 3, 2.5x gain */
-        {{0.376, 0.605}, {0.751, 1.210}, {1.502, 2.420}, {3.004, 4.840}},
-        /* GAIN_SEL = 4, 2x gain */
-        {{0.300, 0.484}, {0.601, 0.968}, {1.202, 1.936}, {2.403, 3.872}},
-        /* GAIN_SEL = 5, 1.667x gain */
-        {{0.250, 0.403}, {0.501, 0.807}, {1.001, 1.613}, {2.003, 3.227}},
-        /* GAIN_SEL = 6, 1.333x gain */
-        {{0.200, 0.323}, {0.401, 0.645}, {0.801, 1.291}, {1.602, 2.581}},
-        /* GAIN_SEL = 7, 1x gain */
-        {{0.150, 0.242}, {0.300, 0.484}, {0.601, 0.968}, {1.202, 1.936}},
-    },
-
-    /* HALLCONF = 0x0 */
-    {
-        /* GAIN_SEL = 0, 5x gain */
-        {{0.787, 1.267}, {1.573, 2.534}, {3.146, 5.068}, {6.292, 10.137}},
-        /* GAIN_SEL = 1, 4x gain */
-        {{0.629, 1.014}, {1.258, 2.027}, {2.517, 4.055}, {5.034, 8.109}},
-        /* GAIN_SEL = 2, 3x gain */
-        {{0.472, 0.760}, {0.944, 1.521}, {1.888, 3.041}, {3.775, 6.082}},
-        /* GAIN_SEL = 3, 2.5x gain */
-        {{0.393, 0.634}, {0.787, 1.267}, {1.573, 2.534}, {3.146, 5.068}},
-        /* GAIN_SEL = 4, 2x gain */
-        {{0.315, 0.507}, {0.629, 1.014}, {1.258, 2.027}, {2.517, 4.055}},
-        /* GAIN_SEL = 5, 1.667x gain */
-        {{0.262, 0.422}, {0.524, 0.845}, {1.049, 1.689}, {2.097, 3.379}},
-        /* GAIN_SEL = 6, 1.333x gain */
-        {{0.210, 0.338}, {0.419, 0.676}, {0.839, 1.352}, {1.678, 2.703}},
-        /* GAIN_SEL = 7, 1x gain */
-        {{0.157, 0.253}, {0.315, 0.507}, {0.629, 1.014}, {1.258, 2.027}},
-    }};
-
-/** Lookup table for conversion time based on [DIF_FILT][OSR].
- */
-const float mlx90393_tconv[8][4] = 
-{
-    /* DIG_FILT = 0 */
-    {1.27, 1.84, 3.00, 5.30},
-    /* DIG_FILT = 1 */
-    {1.46, 2.23, 3.76, 6.84},
-    /* DIG_FILT = 2 */
-    {1.84, 3.00, 5.30, 9.91},
-    /* DIG_FILT = 3 */
-    {2.61, 4.53, 8.37, 16.05},
-    /* DIG_FILT = 4 */
-    {4.15, 7.60, 14.52, 28.34},
-    /* DIG_FILT = 5 */
-    {7.22, 13.75, 26.80, 52.92},
-    /* DIG_FILT = 6 */
-    {13.36, 26.04, 51.38, 102.07},
-    /* DIF_FILT = 7 */
-    {25.65, 50.61, 100.53, 200.37},
-};
-
 union mlx90393_status
 {
     struct
@@ -191,97 +125,97 @@ union mlx90393_status
     /** D1-D0: indicates the number of bytes (2D[1:0]) to follow the status byte after a read measurement
      * or a read register command has been sent.
      */
-    uint8_t d0 : 1;
+    rt_uint8_t d0 : 1;
     /** D1-D0: indicates the number of bytes (2D[1:0]) to follow the status byte after a read measurement
      * or a read register command has been sent.
      */
-    uint8_t d1 : 1;
+    rt_uint8_t d1 : 1;
 
     /** RS: indicates that the device has been reset successfully by a reset command.
      */
-    uint8_t rs : 1;
+    rt_uint8_t rs : 1;
 
     /** SED: indicates that a single bit error has been corrected by the NVRAM
      */
-    uint8_t sed : 1;
+    rt_uint8_t sed : 1;
 
     /** ERROR: indicates an error.
      * Can be set when reading out a measurement while the measurement is not yet completed or
      * when reading out the same measurement twice.
      */
-    uint8_t error : 1;
+    rt_uint8_t error : 1;
 
     /** SM_mode: if set, the IC is executing a measurement sequence in polling mode.
      * It can be initiated by a SM command or a pulse on the TRIG input.
      */
-    uint8_t sm_mode : 1;
+    rt_uint8_t sm_mode : 1;
 
     /** WOC_mode: if set, the IC is in wake-up-on-change mode.
      */
-    uint8_t woc_mode : 1;
+    rt_uint8_t woc_mode : 1;
 
     /** Burst_mode: if set, the IC is working in burst mode.
      */
-    uint8_t burst_mode : 1;
+    rt_uint8_t burst_mode : 1;
     };
 
-    uint8_t byte_val;
+    rt_uint8_t byte_val;
 };
 
 union mlx90393_register0
 {
-    uint16_t word_val;
+    rt_uint16_t word_val;
 
     struct
     {
-        uint8_t hallconf            : 4;
-        uint8_t gain_sel            : 3;
-        uint8_t z_series            : 1;
-        uint8_t bist                : 1;
-        uint8_t ana_reserved_low    : 7;
+        rt_uint8_t hallconf            : 4;
+        rt_uint8_t gain_sel            : 3;
+        rt_uint8_t z_series            : 1;
+        rt_uint8_t bist                : 1;
+        rt_uint8_t ana_reserved_low    : 7;
     };
 };
 
 union mlx90393_register1
 {
-    uint16_t word_val;
+    rt_uint16_t word_val;
 
     struct
     {
-        uint8_t burst_data_rate     : 6;
-        uint8_t burst_sel           : 4;
-        uint8_t tcmp_en             : 1;
-        uint8_t ext_trg             : 1;
-        uint8_t woc_diff            : 1;
-        uint8_t comm_mode           : 2;
-        uint8_t trig_int            : 1;
+        rt_uint8_t burst_data_rate     : 6;
+        rt_uint8_t burst_sel           : 4;
+        rt_uint8_t tcmp_en             : 1;
+        rt_uint8_t ext_trg             : 1;
+        rt_uint8_t woc_diff            : 1;
+        rt_uint8_t comm_mode           : 2;
+        rt_uint8_t trig_int            : 1;
     };
 };
 
 union mlx90393_register2
 {
-    uint16_t word_val;
+    rt_uint16_t word_val;
 
     struct
     {
-        uint8_t osr         : 2;
-        uint8_t dig_filt    : 3;
-        uint8_t res_x       : 2;
-        uint8_t res_y       : 2;
-        uint8_t res_z       : 2;
-        uint8_t osr2        : 2;
-        uint8_t reserved    : 3;
+        rt_uint8_t osr         : 2;
+        rt_uint8_t dig_filt    : 3;
+        rt_uint8_t res_x       : 2;
+        rt_uint8_t res_y       : 2;
+        rt_uint8_t res_z       : 2;
+        rt_uint8_t osr2        : 2;
+        rt_uint8_t reserved    : 3;
     };
 };
 
 union mlx90393_register3
 {
-    uint16_t word_val;
+    rt_uint16_t word_val;
 
     struct
     {
-        uint8_t sens_tc_lt  : 8;
-        uint8_t sens_tc_ht  : 8;
+        rt_uint8_t sens_tc_lt  : 8;
+        rt_uint8_t sens_tc_ht  : 8;
     };
 };
 
